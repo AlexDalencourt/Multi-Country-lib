3fd52ec8edab2eaf52d974ef5f1a72c5db4249bc
adalencourt <adalencourt@norauto.com>
2022-05-29 12:25:05 +0200

    Initial commit

---
+++ src/main/java/com/multi/country/lib/core/annotations/Country.java
@@ -0,0 +1,12 @@
+package com.multi.country.lib.core.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Country {
+    String value();
+}
